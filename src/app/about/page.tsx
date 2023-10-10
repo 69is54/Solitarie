45831030253775c9d0600fcb3b33b65da0e0aa94
chienliko66@gmail.com <clwork1324@gmail.com>
2023-10-10 12:09:01 +0800

    feat: nav

---
+++ src/app/about/page.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div>
+      <div className="container">
+        <h2>Welcome to Our Website</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget dui vel nisi
+          varius tempus. Curabitur vel ante ac lectus cursus luctus eget in ligula. Nam
+          suscipit nunc vel velit fringilla, ut blandit lorem ullamcorper. Vestibulum ante
+          ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas
+          elementum erat in luctus congue.
+        </p>
+        <p>
+          Learn more about our services and products on our{' '}
+          <a href="/">Home Page</a>.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default AboutPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,12 @@ export default function RootLayout({
           </h1>
         </header>
         <section>
-          <nav>nav here</nav>
+        <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+        </ul>
+    </nav>
           {children}
         </section>
       </body>
